a5688e6d93efa9c5d6fed637aaa56057d70bd20a
Adeline Izri <adeline.rudnicki@gmail.com>
2026-09-01 15:45:18 +0200

    feat(routing): fetch restaurant data from slug params
    closes #7

--- src/app/restaurant/[slug]/page.jsx
+++ src/app/restaurant/[slug]/page.jsx
@@ -1,7 +1,12 @@
-export default function RestaurantPage() {
+import { restaurants } from "@/data/restaurants.json";
+
+export default async function RestaurantPage({ params }) {
+  const { slug } = await params;
+  const restaurant = restaurants.find((r) => r.slug === slug);
+
   return (
     <div>
-      <h1>Page restaurant</h1>
+      <h1>{restaurant.name}</h1>
     </div>
   );
 }
